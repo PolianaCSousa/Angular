b32c942d21228c64b063927c26d7743c0b81ad61
Poliana <polianasamonte@gmail.com>
2023-06-23 21:38:23 -0300

    Interpolação de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent {
-
+  //as propriedades podem ser variaveis, arrays ou objetos. Independente do que seja, eu consigo ter acesso a elas lá no template usando {{}}
+  nome :string = "Poliana";
+  disciplinas = ['SD', 'Filosofia', 'Dev. Front'];
+  pet = {
+    nome: 'Bob',
+    idade: 15,
+    raca: 'Poodle'
+  };
 }
